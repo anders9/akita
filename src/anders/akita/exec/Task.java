1a8eb1aeeb417bbb5b302d529e149defb46378f2
anders <anders8@qq.com>
2013-07-23 18:11:05 +0800

    parser

---
+++ src/anders/akita/exec/Task.java
@@ -0,0 +1,16 @@
+package anders.akita.exec;
+
+import java.util.*;
+
+public class Task {
+
+	ArrayList<String> tmpTabs;
+	
+	String sql;
+	String distrTab;
+	
+	String mergeSql;
+	
+	String resultTab;
+	
+}
