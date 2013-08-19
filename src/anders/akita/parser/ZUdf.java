f944c918525f8441a3f8c27cb4f9c594f94d26b7
anders <anders8@qq.com>
2013-08-19 18:31:26 +0800

    parser.

--- src/anders/akita/parser/ZUdf.java
+++ src/anders/akita/parser/ZUdf.java
@@ -4,6 +4,12 @@ import java.util.*;
 
 public class ZUdf {
 
+	public String name;
+	
+	public ArrayList<String> parm;
+	
+	public String exp;
+	
 	public ZUdf(String name, ArrayList<String> parm, ZExp exp){
 		
 	}
