451f5dd1e363ff8dafab9c2ba1705b5caca69f91
anders <anders8@qq.com>
2013-09-15 23:42:57 +0800

    exec & join

--- src/anders/akita/exec/JoinChain.java
+++ src/anders/akita/exec/JoinChain.java
@@ -1,15 +1,20 @@
 package anders.akita.exec;
 
+import anders.akita.parser.*;
 import java.util.*;
 
 public class JoinChain {
-
+	
 	JoinChain prev;
 	
-	ArrayList<String> left;
+	ArrayList<String> fromTabs;//used when JoinChain.prev==NULL
+	String distrTab;//May be NULL,//used when JoinChain.prev==NULL
 	
-	ArrayList<String> right;
+	//The following properties is used when JoinChain.prev != NULL
+	String rhsTab;
 	
+	ArrayList<String> rhsLocalTabs;
+	ArrayList<RootExp> joinConds;
 	int join_type;
 	
 	int optType;
